b733cee8b7a8f2998d32f8b2e5681ea529064ded
RubBen19 <98461533+RubBen19@users.noreply.github.com>
2025-01-09 08:46:38 +0100

    Create ShotInformScreenUiState.kt
    Not tested, wrote directly from GitHub Web

---
+++ app/src/main/java/com/example/basket4all/presentation/uistate/ShotInformScreenUiState.kt
@@ -0,0 +1,30 @@
+data ShotInformScreenUiState(
+  // Media distancia
+  val 2pLeftCornerIn: Int = 0,
+  val 2pLeftCornerOut: Int = 0,
+  val 2pLeftCornerTotal: Int = 0,
+  val 2pLeftCornerPercent: Float = 0.0f,
+  // Triple
+  val 3pLeftCornerIn: Int = 0,
+  val 3pLeftCornerOut: Int = 0,
+  val 3pLeftCornerTotal: Int = 0,
+  val 3pLeftCornerPercent: Float = 0.0f,
+  // Desde la zona
+  val zoneLeftIn: Int = 0,
+  val zoneLeftOut: Int = 0,
+  val zoneLeftTotal: Int = 0,
+  val zoneLeftPercent: Float = 0.0f,
+  val zoneCenterIn: Int = 0,
+  val zoneCenterOut: Int = 0,
+  val zoneCenterTotal: Int = 0,
+  val zoneCenterPercent: Float = 0.0f,
+  val zoneRightIn: Int = 0,
+  val zoneRightOut: Int = 0,
+  val zoneRightTotal: Int = 0,
+  val zoneRightPercent: Float = 0.0f,
+  // Tiros libres
+  val freeShIn: Int = 0,
+  val freeShOut: Int = 0,
+  val freeShTotal: Int = 0,
+  val freeShPercent: Float = 0.0f
+)
