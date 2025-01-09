c8fe74bc2012ae73d6834c74de1b4f2485abfc10
RubBen19 <98461533+RubBen19@users.noreply.github.com>
2025-01-09 08:57:44 +0100

    Create MiLigueScreenUiState.kt
    Not tested, wrote directly from github web

---
+++ app/src/main/java/com/example/basket4all/presentation/uistate/MiLigueScreenUiState.kt
@@ -0,0 +1,10 @@
+data class MiLigueScreenUiState(
+  // Datos generales de la liga
+  val name: String = "",
+  val category: String = "",
+  val division: String = "",
+  // Lista de equipos
+  val rivalsTeamsList: List<TeamEntity> = listOf()
+  // Flag de carga de la pantalla
+  val loading: Boolean = false
+)
